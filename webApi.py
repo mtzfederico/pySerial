4567071497e5abc2ead33d5ef5abd9cb9ae19b8c
Fede Mtz <44717422+mtzfederico@users.noreply.github.com>
2019-12-04 20:57:55 -0600

    Create webApi.py

---
+++ webApi.py
@@ -0,0 +1,28 @@
+#https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
+import time
+from flask import Flask, render_template, request
+import serial
+ser = serial.Serial('/dev/tty.usbmodem14301', 115200)
+app = Flask(__name__)
+
+#@app.route("/") #This part is supposed to print an html file, but it doesn't work
+#def index():
+    #return render_template('/index.html')
+@app.route("/<deviceName>/")
+def action(deviceName):
+    if deviceName == 'red':
+        ser.write("red")
+        return "<p>red led on</p>"
+    if deviceName == 'green':
+        ser.write("green")
+        return "<p>green led on</p>"
+    if deviceName == 'blue':
+        ser.write("blue")
+        return "<p>blue led on</p>"
+    if deviceName == 'off':
+        ser.write("off")
+        return "<p>led off</p>"
+    #return render_template('/index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=False)
