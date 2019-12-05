2d1074fcff0cb8112a85403d8029b5f718e694ac
Fede Mtz <44717422+mtzfederico@users.noreply.github.com>
2019-12-04 21:00:04 -0600

    Update webApi.py

--- webApi.py
+++ webApi.py
@@ -2,12 +2,13 @@
 import time
 from flask import Flask, render_template, request
 import serial
-ser = serial.Serial('/dev/tty.usbmodem14301', 115200)
+ser = serial.Serial('/dev/tty.usbmodem14301', 115200) # Establish the connection on a specific port
 app = Flask(__name__)
 
-#@app.route("/") #This part is supposed to print an html file, but it doesn't work
-#def index():
-    #return render_template('/index.html')
+@app.route("/") #This part is supposed to print an html file, but it doesn't work
+def index():
+    return "<p>Arduino pySerial API</p>"
+    #return render_template('/index.html') #This part is supposed to print an html file, but it doesn't work
 @app.route("/<deviceName>/")
 def action(deviceName):
     if deviceName == 'red':
